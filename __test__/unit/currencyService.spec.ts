7dc5efce042cf81251bc963be9a871dd19387b06
fer <ferp@protonmail.ch>
2020-06-24 20:24:15 +0200

    initiated the unit-test spec for the currency service

---
+++ __test__/unit/currencyService.spec.ts
@@ -0,0 +1,9 @@
+import { CurrencyService } from '../../src/domain/CurrencyService'
+//import { CurrencyOracleStub }
+
+describe('currency service', () => {
+    it('has a running test specification', () => {
+        expect(true).toBe(true)
+    })
+    xit('gets a currency list from the oracle', () => {})
+})
